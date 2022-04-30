964e62471bbad709e481f83f1b03b39ac8d5a602
iamyeasirarafat <yas.yeasir@gmail.com>
2022-05-01 01:12:52 +0600

    getting post

--- index.js
+++ index.js
@@ -4,19 +4,53 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+
+
 //midlware
 app.use(cors());
 app.use(express.json());
 
+
+// mongoDb 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@uttaramotors.4k8ax.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
- console.log('connected Database');
-  client.close();
-});
-app.get('/', (req, res) => {
-    res.send('this is test that server is runnig')
-})
+
+
+// Connect with the Database Server
+ const run = async() => {
+    try {
+        await client.connect();
+        const productsData = client.db('UttaraMotors').collection('Products');
+
+        //get all products
+        app.get('/', async (req, res) => {
+            const query ={};
+            const cursor = await productsData.find(query).toArray()
+            res.send(cursor);
+        })
+
+    } 
+    finally {
+        
+    }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(port , ()=>{
     console.log('listening to the port', port);
 })
