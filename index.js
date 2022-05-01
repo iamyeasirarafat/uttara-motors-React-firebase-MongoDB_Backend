7010c48d1e3e4c48a78bcb3fd62a92f317ca74f6
iamyeasirarafat <yas.yeasir@gmail.com>
2022-05-02 02:24:26 +0600

    blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const port = process.env.PORT || 5000;
 //midlware
 app.use(cors());
 app.use(express.json());
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
 // mongoDb 
 
